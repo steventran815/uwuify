44ee1b2cd43244163a9e6886feb28465029ac939
Steven Tran <steventran@stevens-mbp.lan>
2021-04-14 23:05:02 -0700

    Updated code to use wordTable | Moved Glitter Function

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 var button = document.getElementById("onClick");
 var output = document.getElementById("output");
 var input = document.getElementById("formInput");
@@ -15,7 +17,7 @@ var outputBox = document.getElementById("outputBox");
 var sway = document.getElementById("sway");
 var sway2 = document.getElementById("sway2");
 var resetButton = document.getElementById("resetButton")
-var angryCat=document.getElementById("angryCat")
+var angryCat = document.getElementById("angryCat")
 var animeSong1 = new Audio("sounds/animeSong1.mp3");
 var animeSong2 = new Audio("sounds/animeSong2.mp3");
 var animeSong3 = new Audio("sounds/animeSong3.mp3");
@@ -26,7 +28,7 @@ var glitterSound = new Audio("sounds/glitter.mp3");
 // var voices = window.speechSynthesis.getVoices();
 var songArray = [animeSong1, animeSong2, animeSong3, animeSong4, animeSong5];
 button.addEventListener("click", uwuify);
-resetButton.addEventListener("click",reset);
+resetButton.addEventListener("click",weset);
 
 var randomSongNumber = Math.floor(
   Math.random() * Math.floor(songArray.length)
@@ -54,78 +56,47 @@ function uwuify() {
     return;
   }
 
-  if(inputValue.includes("angry")){
-    angryCat.classList.remove("hidden-cat")
+  if (inputValue.includes("angry")) {
+    angryCat.classList.remove("hidden-cat");
   }
 
   var splitInput = inputValue.toLowerCase().split(" ");
   var randomNumber = Math.floor(Math.random() * Math.floor(splitInput.length));
+
   var randomWord = splitInput[randomNumber];
   var newRandomWord = randomWord[0] + "-" + randomWord;
   splitInput[randomNumber] = newRandomWord;
 
-  for (var i = 0; i < splitInput.length; i++) {
-    var word = splitInput[i]
-    if (word === "hi") {
-      word = "ohai";
-    }
-    if (word === "this"){
-      word = "dis"
-    }
-    if (word === "i") {
-      word = "me";
-    }
-    if (word === "your") {
-      word = "ur";
-    }
-    if (
-      word === "its" ||
-      word === "it's" ||
-      word === "is" ||
-      word === "am"
-    ) {
-      word = "ish";
-    }
-    if (word === "hungry") {
-      word = "hungy";
-    }
-    if (word === "you") {
-      word = "yuwu";
-    }
-    if (word === "please") {
-      word = "pweez";
-    }
-    if (word === "cute") {
-      word = "kawaii desune";
-    }
-    if (word === "have") {
-      word = "hab";
-    }
-    if (word === "give") {
-      word = "gib";
-    }
-    if (word === "hey") {
-      word = "ohayo";
-    }
-    if (word === "stop") {
-      word = "stahp";
-    }
-    if (word === "the") {
-      word = "da";
-    }
-    if (word === "lol") {
-      word = "wwwwwww";
-    }
-    if (word === "angry") {
-      word = "angy 😾";
-    }
-    if (word === "gelowaggle") {
-      word = "gewo-senpai";
-    }
-    if (word === "love"){
-      word = "wuv"
-    }
+  var wordTable = {
+    hi: "ohai",
+    this: "dis",
+    i: "me",
+    your: "ur",
+    its: "ish",
+    "it's": "ish",
+    is: "ish",
+    am: "ish",
+    hungry: "hungy",
+    you: "yuwu",
+    please: "pweez",
+    cute: "kawaii desune",
+    have: "hab",
+    give: "gib",
+    hey: "ohayo",
+    stop: "stahp",
+    the: "da",
+    lol: "wwwwwww",
+    angry: "angy 😾",
+    gelowaggle: "gewo-senpai",
+    love: "wuv",
+    sad: "sadge",
+  };
 
+  for (var i = 0; i < splitInput.length; i++) {
+    var word = splitInput[i];
+    if (wordTable[word]) {
+      word = wordTable[word];
+    }
 
     for (var j = 0; j < word.length - 1; j++) {
       var newWord = word
@@ -140,152 +111,21 @@ function uwuify() {
         emptyArray.push(newWord[j]);
       }
     }
-    if(word.length === 1 ){
-      emptyArray.push(word)
+    if (word.length === 1) {
+      emptyArray.push(word);
     } else {
       emptyArray.push(newWord[word.length - 1]);
     }
     emptyArray.push(" ");
-    output.textContent =
-      emptyArray.join("") + " uwu";
+    output.textContent = emptyArray.join("") + " uwu";
   }
   updateDOM();
 }
 
-(function () {
-  var COLORS,
-    Confetti,
-    NUM_CONFETTI,
-    PI_2,
-    canvas,
-    confetti,
-    context,
-    drawCircle,
-    g,
-    range,
-    resizeWindow,
-    xpos;
-
-  NUM_CONFETTI = 350;
-
-  COLORS = [
-    [255, 255, 255],
-    [255, 105, 180],
-    [255, 252, 187],
-    [255, 105, 180],
-    [255, 255, 255],
-  ];
-
-  PI_2 = 2 * Math.PI;
-  canvas = document.getElementById("world");
-  context = canvas.getContext("2d");
-  window.w = 0;
-  window.h = 0;
-  resizeWindow = function () {
-    window.w = canvas.width = window.innerWidth;
-    return (window.h = canvas.height = window.innerHeight);
-  };
-
-  window.addEventListener("resize", resizeWindow, false);
-  window.onload = function () {
-    return setTimeout(resizeWindow, 0);
-  };
-  range = function (a, b) {
-    return (b - a) * Math.random() + a;
-  };
-  drawCircle = function (x, y, r, style) {
-    context.beginPath();
-    context.arc(x, y, r, 0, PI_2, false);
-    context.fillStyle = style;
-    return context.fill();
-  };
-  xpos = 0.5;
-  document.onmousemove = function (e) {
-    return (xpos = e.pageX / w);
-  };
-  window.requestAnimationFrame = (function () {
-    return (
-      window.requestAnimationFrame ||
-      window.webkitRequestAnimationFrame ||
-      window.mozRequestAnimationFrame ||
-      window.oRequestAnimationFrame ||
-      window.msRequestAnimationFrame ||
-      function (callback) {
-        return window.setTimeout(callback, 1000 / 60);
-      }
-    );
-  })();
-  Confetti = class Confetti {
-    constructor() {
-      this.style = COLORS[~~range(0, 5)];
-      this.rgb = `rgba(${this.style[0]},${this.style[1]},${this.style[2]}`;
-      this.r = ~~range(2, 6);
-      this.r2 = 2 * this.r;
-      this.replace();
-    }
-    replace() {
-      this.opacity = 0;
-      this.dop = 0.03 * range(1, 4);
-      this.x = range(-this.r2, w - this.r2);
-      this.y = range(-20, h - this.r2);
-      this.xmax = w - this.r;
-      this.ymax = h - this.r;
-      this.vx = range(0, 2) + 8 * xpos - 5;
-      return (this.vy = 0.7 * this.r + range(-1, 1));
-    }
-    draw() {
-      var ref;
-      this.x += this.vx;
-      this.y += this.vy;
-      this.opacity += this.dop;
-      if (this.opacity > 1) {
-        this.opacity = 1;
-        this.dop *= -1;
-      }
-      if (this.opacity < 0 || this.y > this.ymax) {
-        this.replace();
-      }
-      if (!(0 < (ref = this.x) && ref < this.xmax)) {
-        this.x = (this.x + this.xmax) % this.xmax;
-      }
-      return drawCircle(
-        ~~this.x,
-        ~~this.y,
-        this.r,
-        `${this.rgb},${this.opacity})`
-      );
-    }
-  };
-  confetti = (function () {
-    var j, ref, results;
-    results = [];
-    for (
-      g = j = 1, ref = NUM_CONFETTI;
-      1 <= ref ? j <= ref : j >= ref;
-      g = 1 <= ref ? ++j : --j
-    ) {
-      results.push(new Confetti());
-    }
-    return results;
-  })();
-  window.step = function () {
-    var c, j, len, results;
-    requestAnimationFrame(step);
-    context.clearRect(0, 0, w, h);
-    results = [];
-    for (j = 0, len = confetti.length; j < len; j++) {
-      c = confetti[j];
-      results.push(c.draw());
-    }
-    return results;
-  };
-  step();
-
-}.call(this));
 function updateDOM() {
   var msg = new SpeechSynthesisUtterance(output.textContent);
   msg.volume = 0.6; // 0 to 1
-  msg.lang = "ko-JP";
+  msg.lang = "ja-JP";
   msg.pitch = 1.25;
   setTimeout(function () {
     window.speechSynthesis.speak(msg);
@@ -318,7 +158,7 @@ function updateDOM() {
   outputBox.classList.add("reveal");
 }
 
-function reset(){
+function weset(){
   window.location.reload();
 }
 
